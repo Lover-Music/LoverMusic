33b19e31475288d3f6650abf53a6d00feccd7aa4
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-02-17 15:50:22 +0530

    Update __init__.py

--- LoverMusic/__init__.py
+++ LoverMusic/__init__.py
@@ -1,17 +1,8 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
-from YukkiMusic.core.bot import YukkiBot
-from YukkiMusic.core.dir import dirr
-from YukkiMusic.core.git import git
-from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku, sudo
+from LoverMusic.core.bot import LoverBot
+from LoverMusic.core.dir import dirr
+from LoverMusic.core.git import git
+from LoverMusic.core.userbot import Userbot
+from LoverMusic.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
